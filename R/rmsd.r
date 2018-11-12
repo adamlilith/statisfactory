6ec1f7e350a11a7b518eeedb84a4b14d8fcf6ab6
Adam B. Smith <adam@earthskysea.org>
2018-11-11 23:41:55 -0600

    Add weights

--- R/rmsd.r
+++ R/rmsd.r
@@ -1,8 +1,9 @@
 #' Root-mean-square deviation
 #'
-#' Calculate the root-mean-square deviation (\code{sqrt(mean((x1 - x2)^2))}).
+#' Calculate the root-mean-square deviation (\code{sqrt(mean((x1 - x2)^2))}). If non-constant weights \code{w} are supplied, then the calculation is \code{sqrt(sum(w * (x1 - x2)^2) / sum(w))}.
 #' @param x1 Numeric vector.
 #' @param x2 Numeric vector same length as \code{x1}.
+#' @param w Numeric vector of weights same length as \code{x1} and \code{x2}. The default is to assign each pair of values in \code{x1} and \code{x2} equal weight.
 #' @param na.rm Logical, if \code{TRUE} then remove any elements in \code{x1} \emph{and} \code{x2} where either \code{x1} or \code{x2} is \code{NA}. Default is \code{FALSE}, in which case any \code{NA} returns \code{NA}.
 #' @return Numeric.
 #' @examples
@@ -18,6 +19,7 @@
 rmsd <- compiler::cmpfun(function(
 	x1,
 	x2,
+	w = length(x1),
 	na.rm = FALSE
 ) {
 
@@ -26,13 +28,16 @@ rmsd <- compiler::cmpfun(function(
 	}
 
 	if (na.rm) {
-		cleaned <- naOmitMulti(x1, x2)
+		cleaned <- naOmitMulti(x1, x2, w)
 		x1 <- cleaned[[1]]
 		x2 <- cleaned[[2]]
+		w <- cleaned[[3]]
 	}
 	
+	totalWeight <- sum(w)
+	
 	out <- if (length(x1) > 0 & length(x2) > 0) {
-		sqrt(mean((x1 - x2)^2))
+		sqrt(sum(w * (x1 - x2)^2) / totalWeight)
 	} else {
 		NA
 	}
